05ec65f233eba83d037f33b273014dd957bec087
Marcos Martini <marcos@kinde.com>
2023-06-09 13:39:36 +1000

    test

--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -6,19 +6,16 @@ export default function LoggedIn() {
   return (
     <>
       <header>
+        {console.log(user)}
         <nav className="nav container">
           <h1 className="text-display-3">KindeAuth</h1>
           <div className="profile-blob">
-            {user.picture !== "" ? (
-              <img
-                className="avatar"
-                src={user.picture}
-                alt="user profile avatar"
-              />
+            {user.picture ? (
+              <img className="avatar" src={user.picture} alt="user profile avatar" />
             ) : (
               <div className="avatar">
                 {user?.given_name?.[0]}
-                {user?.family_name?.[1]}
+                {user?.family_name?.[0]}
               </div>
             )}
             <div>
@@ -59,9 +56,7 @@ export default function LoggedIn() {
             </a>
           </p>
 
-          <small className="text-subtle">
-            © 2022 KindeAuth, Inc. All rights reserved
-          </small>
+          <small className="text-subtle">© 2022 KindeAuth, Inc. All rights reserved</small>
         </div>
       </footer>
     </>
